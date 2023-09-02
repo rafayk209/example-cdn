3dab5d25c36ceec27f14f99d6375975bdf71d20a
rafayk209 <rafayk209@gmail.com>
2023-09-02 11:53:33 +0500

    .js

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,29 @@
+const path = require('path');
+
 module.exports = {
-    entry: './src/index.js', // Your React.js entry file
-    output: {
-      filename: 'bundle.js', // Output JavaScript file name
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          use: 'babel-loader', // Use Babel for JS files
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.css$/,
-          use: [
-            {
-              loader: 'style-loader', // Inject styles into the DOM
-            },
-            {
-              loader: 'css-loader', // Handle CSS files
-              options: {
-                modules: true, // Enable CSS modules if needed
-              },
-            },
-          ],
-        },
-      ],
-    },
-  };
-  
+  entry: './src/index.js', // Your React.js entry file
+  output: {
+    filename: 'bundle.js', // Output JavaScript file name
+    path: path.resolve(__dirname, 'dist'), // Output directory (create a 'dist' folder)
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader', // Use Babel for JS files
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader', // Inject styles into the DOM
+          },
+          {
+            loader: 'css-loader', // Handle CSS files
+          },
+        ],
+      },
+    ],
+  },
+};
